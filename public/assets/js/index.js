62e5ccf8cbe2e61d96ead718794255bcfdc5c32e
Joe Kozlowski <kozineffect@github.com>
2017-06-14 13:03:40 -0400

    Just farted around with stuff

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,14 +1,14 @@
 $(document).ready(function () {
-    var firstName = $("#first_name");
-    var lastName = $("#last_name");
-    var email = $("#email");
-    var username = $("#username");
-    var password = $("#password");
-    var genres = $(".form-check-label");
-    var consoles=$("");
-    var fav1 = $("#fav1");
-    var fav2 = $("#fav2");
-    var fav3 = $("#fav3");
+    var firstName = $("#first_name").val().trim();
+    var lastName = $("#last_name").val().trim();
+    var email = $("#email").val().trim();
+    var username = $("#username".val().trim());
+    var password = $("#password").val().trim();
+    var genres = $(".form-check-label").val();
+    var consoles=$("consoles").val().trim();
+    var fav1 = $("#fav1").val().trim();
+    var fav2 = $("#fav2").val().trim();
+    var fav3 = $("#fav3").val().trim();
     
     var userForm = $("#newUserForm");
 
@@ -22,16 +22,16 @@ $(document).ready(function () {
 
 
         var newPost = {
-            first_name: firstName.val().trim(),
-            last_name: lastName.val().trim(),
-            email: email.val().trim(),
-            username: username.val().trim(),
-            password: password.val().trim(),
-            genres: genres.val(),
-            FavGame1: fav1.val().trim(),
-            FavGame2: fav2.val().trim(),
-            FavGame3: fav3.val().trim(),
-            consoles: consoles.val()
+            first_name: firstName,
+            last_name: lastName,
+            email: email,
+            username: username,
+            password: password,
+            genres: genres,
+            FavGame1: fav1,
+            FavGame2: fav2,
+            FavGame3: fav3,
+            consoles: consoles
             
         };
         submitPost(newPost);
